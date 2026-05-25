2bd6341899973e58cea30fe119da60fab9997e77
SinanAhmetZeren <SinanAhmetZeren@gmail.com>
2026-05-25 12:54:34 +0300

    Refactor unread tracking: DB-driven badge, reset on message fetch, remove cache usage

--- s.sh
+++ s.sh
@@ -11,38 +11,45 @@ end tell
 
 set hw to sw / 2
 set h1 to sh / 8
-set h4 to sh / 2
 set h3 to (sh * 3) / 8
+set h2 to sh / 4
+set h35 to (sh * 7) / 16
 set menuBar to 25
 
 tell application "Terminal"
-    -- SSH tunnel - left, 1/8 height
+    -- SSH tunnel - left, 1/8
     do script "ssh -N -L 5432:127.0.0.1:5432 deploy@168.119.119.25"
     delay 0.5
     set bounds of front window to {0, menuBar, hw, menuBar + h1}
-    delay 3
+    delay 0.5
 
-    -- API - left, 4/8 height
+    -- API - left, 3/8
     do script "cd /Users/ahmetzeren/Repos/parrotsAPI2 && dotnet run"
     delay 0.5
-    set bounds of front window to {0, menuBar + h1, hw, menuBar + h1 + h4}
+    set bounds of front window to {0, menuBar + h1, hw, menuBar + h1 + h3}
     delay 0.3
 
-    -- Web - left, 1/8 height
-    do script "cd /Users/ahmetzeren/Repos/parrotsWeb && npm start"
+    -- Web - left, 1/8
+    do script "cd /Users/ahmetzeren/Repos/parrotsWeb && BROWSER=none npm start"
     delay 0.5
-    set bounds of front window to {0, menuBar + h1 + h4, hw, menuBar + h1 + h4 + h1}
+    set bounds of front window to {0, menuBar + h1 + h3, hw, menuBar + h1 + h3 + h1}
     delay 0.3
 
-    -- ngrok - right, 3/8 height
+    -- ngrok - left, 2/8
     do script "pkill ngrok; sleep 1; ngrok http --domain=adapting-sheepdog-annually.ngrok-free.app https://localhost:7151"
     delay 0.5
-    set bounds of front window to {hw, menuBar, sw, menuBar + h3}
+    set bounds of front window to {0, menuBar + h1 + h3 + h1, hw, menuBar + h1 + h3 + h1 + h2}
     delay 0.3
 
-    -- Mobile - right, 3/8 height
-    do script "cd /Users/ahmetzeren/Repos/parrotsapp && npx expo start"
+    -- Mobile 1 - right, 3.5/8
+    do script "cd /Users/ahmetzeren/Repos/parrotsapp && npx expo start --port 8081"
     delay 0.5
-    set bounds of front window to {hw, menuBar + h3, sw, menuBar + h3 + h3}
+    set bounds of front window to {hw, menuBar, sw, menuBar + h35}
+    delay 0.3
+
+    -- Mobile 2 - right, 3.5/8
+    do script "cd /Users/ahmetzeren/Repos/parrotsapp && npx expo start --port 8082"
+    delay 0.5
+    set bounds of front window to {hw, menuBar + h35, sw, menuBar + h35 + h35}
 end tell
 EOF
