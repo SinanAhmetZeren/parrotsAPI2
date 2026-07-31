2bb161b70dc30c064138d28d385a3a72d11c3446
SinanAhmetZeren <SinanAhmetZeren@gmail.com>
2026-07-31 10:46:49 +0300

    Temp: set minVersion to 1.0.20 for testing update modal

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VersionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { minVersion = "1.0.20" });
+    }
+}
