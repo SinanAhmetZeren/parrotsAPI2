b1cded9f77967ed2985b96484daddaf141695757
SinanAhmetZeren <SinanAhmetZeren@gmail.com>
2026-09-13 14:05:48 +0300

    Add migration BioMaxLength700

---
+++ Migrations/20260913110321_BioMaxLength700.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParrotsAPI2.Migrations
+{
+    /// <inheritdoc />
+    public partial class BioMaxLength700 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "AspNetUsers",
+                type: "character varying(700)",
+                maxLength: 700,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "AspNetUsers",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(700)",
+                oldMaxLength: 700,
+                oldNullable: true);
+        }
+    }
+}
